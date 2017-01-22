963eab4070ca24318a4f04ae143b3f6f258b12c0
srkanuri <kanurisrikanth@yahoo.co.in>
2017-01-22 10:34:42 -0500

    removed unwanted fns

--- rfReceiver/rfReceiver.ino
+++ rfReceiver/rfReceiver.ino
@@ -5,7 +5,7 @@ void setup() {
   Serial.begin(9600);
   // Transmitter is connected to Arduino Pin #10  
   mySwitch1.enableReceive(0);
-//}
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -33,25 +33,4 @@ void loop() {
     mySwitch1.resetAvailable();
 }
 delay(200);
-}
-
-static char * dec2binWzerofill(unsigned long Dec, unsigned int bitLength) {
-  static char bin[64]; 
-  unsigned int i=0;
-
-  while (Dec > 0) {
-    bin[32+i++] = ((Dec & 1) > 0) ? '1' : '0';
-    Dec = Dec >> 1;
-  }
-
-  for (unsigned int j = 0; j< bitLength; j++) {
-    if (j >= bitLength - i) {
-      bin[j] = bin[ 31 + i - (j - (bitLength - i)) ];
-    } else {
-      bin[j] = '0';
-    }
-  }
-  bin[bitLength] = '\0';
-  
-  return bin;
 }
